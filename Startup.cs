fcc82efe6ce5673dd4e51fe151b0ae65ac0ea677
Carr <jc086615@ohio.edu>
2020-05-24 14:35:01 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jess_Carr_Entertainment.Startup))]
+namespace Jess_Carr_Entertainment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
